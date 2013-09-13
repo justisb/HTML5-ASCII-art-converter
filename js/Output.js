92493812345dd493eef721732c60bc75fdf7acee
HaruAtari <HaruAtari@gmail.com>
2013-09-13 21:43:18 +0400

    Изменен конструктор в классе Output.

--- js/Output.js
+++ js/Output.js
@@ -1,24 +1,39 @@
 /**
  * @author Pikaev Viktor <HaruAtari@gmail.com> https://github.com/HaruAtari
  */
- var Output = (function() {
+var Output = function () {
     var self = this,
         sourceImageId = 'image-source',
         resultImageId = 'image-result';
 
-    return function() {
-        this.getSourceImageId = function() { return sourceImageId; };
-        this.setSourceImageId = function(id) { return sourceImageId = id; };
-
-        this.getResultImageId = function() { return resultImageId; };
-        this.setResultImageId = function(id) { return resultImageId = id; };
-
-        this.getSourceImage = function() { return document.getElementById(this.getSourceImageId()); };
-
-        this.getResultImage = function() { return document.getElementById(this.getResultImageId()); };
-
-        this.setSourceImageSrc = function(src) { return this.getSourceImage().src = src; }
-
-        this.setResultImageSrc = function(src) { return this.getResultImage().src = src; }
+    this.getSourceImageId = function () {
+        return sourceImageId;
     };
-})();
+    this.setSourceImageId = function (id) {
+        return sourceImageId = id;
+    };
+
+    this.getResultImageId = function () {
+        return resultImageId;
+    };
+    this.setResultImageId = function (id) {
+        return resultImageId = id;
+    };
+
+    this.getSourceImage = function () {
+        return document.getElementById(self.getSourceImageId());
+    };
+
+    this.getResultImage = function () {
+        return document.getElementById(self.getResultImageId());
+    };
+
+    this.setSourceImageSrc = function (src) {
+        return self.getSourceImage().src = src;
+    }
+
+    this.setResultImageSrc = function (src) {
+        return self.getResultImage().src = src;
+    }
+
+};
