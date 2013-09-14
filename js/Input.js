f404fa99bc5b0f5f0627753d6362ad2be67d76d7
HaruAtari <HaruAtari@gmail.com>
2013-09-14 10:11:58 +0400

    Откоментированны классы Input и output.

--- js/Input.js
+++ js/Input.js
@@ -2,16 +2,25 @@
  * @author Pikaev Viktor <HaruAtari@gmail.com> https://github.com/HaruAtari
  */
 var Input = function () {
-    var self = this,
-        inputFieldId = "input-field",
-        allowMimeTypes = [
-            "image/jpg",
-            "image/jpeg",
-            "image/png"
-        ],
-        fileData = null,
+    var self = this;
 
-        loadFile = function (file, callback) {
+    // Id поля выбора исходного файла.
+    var inputFieldId = "input-field";
+
+    // Список допустимых расширений.
+    var allowMimeTypes = [
+        "image/jpg",
+        "image/jpeg",
+        "image/png"
+    ];
+
+    // Загруженный файл в bas64 кодировке
+    var fileData = null;
+
+    // Сохраняет содержимое выбранного файла в переменную fileData.
+    // {file} - Загруженный файл. @see this.loadFromField()
+    // {callback} - Функция, которая будет вызвана после завершения загрузки.
+    var loadFile = function (file, callback) {
             if (self.hasAllowMimeType(file.type) === false) {
                 alert("File has deny mime type.");
                 throw "File has deny mime type.";
@@ -27,30 +36,38 @@ var Input = function () {
             reader.readAsDataURL(file);
         };
 
+    // Загружает файл из поля выбора файла.
     this.loadFromField = function (event, callback) {
         loadFile(event.target.files[0], callback);
     };
 
+    // Возвращает Id поля выбора файла.
     this.getInputFieldId = function () {
         return inputFieldId;
     };
+
+    // Устанавливает Id поля выбора файла.
     this.setInputFieldId = function (id) {
         return inputFieldId = id;
     };
 
+    // Возвращает поле выбора файла.
     this.getInputField = function () {
         return document.getElementById(self.getInputFieldId());
     };
 
+    // Возвращает список допустимых расширений.
     this.getAllowMimeTypes = function () {
         return allowMimeTypes;
     };
 
+    // Проверяет, допустимо ли указанное расширение.
     this.hasAllowMimeType = function (mime) {
         return self.getAllowMimeTypes().indexOf(mime) > -1;
     };
 
+    // Возвращает содержимое загруденного файла.
     this.getFileData = function () {
         return fileData;
-    }
+    };
 };
--- js/Output.js
+++ js/Output.js
@@ -2,38 +2,51 @@
  * @author Pikaev Viktor <HaruAtari@gmail.com> https://github.com/HaruAtari
  */
 var Output = function () {
-    var self = this,
-        sourceImageId = 'image-source',
-        resultImageId = 'image-result';
+    var self = this;
 
+    // Id исходного изображения.
+    var sourceImageId = 'image-source';
+
+    // Id результирующего изображения.
+    var resultImageId = 'image-result';
+
+    // Возвращает Id исходного изображения.
     this.getSourceImageId = function () {
         return sourceImageId;
     };
+
+    // Установливает Id исходного изображения.
     this.setSourceImageId = function (id) {
         return sourceImageId = id;
     };
 
+    // Возвращает Id результирующего изображения.
     this.getResultImageId = function () {
         return resultImageId;
     };
+
+    // Устанавливает Id исходного изображения.
     this.setResultImageId = function (id) {
         return resultImageId = id;
     };
 
+    // Возвращает исходное изображение.
     this.getSourceImage = function () {
         return document.getElementById(self.getSourceImageId());
     };
 
+    // Возвращает результирующее изображение.
     this.getResultImage = function () {
         return document.getElementById(self.getResultImageId());
     };
 
+    // Устанавливает src-аттрибут исходного изображения.
     this.setSourceImageSrc = function (src) {
         return self.getSourceImage().src = src;
-    }
+    };
 
+    // Устанавливает src-аттрибут результирующего изображения.
     this.setResultImageSrc = function (src) {
         return self.getResultImage().src = src;
-    }
-
+    };
 };
